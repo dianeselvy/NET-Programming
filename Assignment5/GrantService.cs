9568c76fb3414ce17db63a98e3ade529e048194c
Diane Selvy <dianeselvy@hotmail.com>
2017-05-11 11:37:53 -0700

    WCF Service Assignment

---
+++ Assignment5/GrantService.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "GrantService" in code, svc and config file together.
+public class GrantService : IGrantService
+{
+    Community_AssistEntities db = new Community_AssistEntities();
+
+    public List<GrantRequest> GetGrant()
+    {
+        var gs = from b in db.GrantRequests
+                     select b;
+
+        List<GrantRequest> grants = new List<GrantRequest>();
+        foreach (var g in gs)
+        {
+            GrantRequest grant1 = new GrantRequest();
+            grant1.PersonKey = g.PersonKey;
+            grant1.GrantRequestDate = g.GrantRequestDate;
+            grant1.GrantRequestExplanation = g.GrantRequestExplanation;
+            grants.Add(grant1);
+        }
+        return grants;
+    }
+
+
+    public List<GrantRequest> GetGrantsByPerson(string personName)
+    {
+        var gs = from b in db.GrantRequests
+                 where b.Person.PersonLastName == personName
+                 select b;
+
+        List<GrantRequest> grants = new List<GrantRequest>();
+        foreach (var g in gs)
+        {
+            GrantRequest grant1 = new GrantRequest();
+            grant1.PersonKey = g.PersonKey;
+            grant1.GrantRequestDate = g.GrantRequestDate;
+            grant1.GrantRequestExplanation = g.GrantRequestExplanation;
+            grants.Add(grant1);
+        }
+        return grants;
+    }
+
+    public int Login(string user, string password)
+    {
+        int key = 0;
+        int result = db.usp_Login(user, password);
+        if(result != -1)
+        {
+            var userKey = (from k in db.People
+                           where k.PersonEmail.Equals(user)
+                           select k.PersonKey).FirstOrDefault();
+        }
+        return key;
+    }
+
+    public bool RegisterPerson(Person p)
+    {
+        bool result = true;
+        int pers = db.usp_Register(p.PersonLastName, p.PersonFirstName, p.PersonEmail,
+            p.PersonPassWord);
+
+        return result;
+    }
+}
---
+++ Assignment5/IGrantService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+// NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IGrantService" in both code and config file together.
+[ServiceContract]
+public interface IGrantService
+{
+    [OperationContract]
+    int Login(string user, string password);
+
+    [OperationContract]
+    bool RegisterPerson(Person p);
+
+    [OperationContract]
+    List<GrantRequest> GetGrant();
+
+    [OperationContract]
+    List<GrantRequest> GetGrantsByPerson(string personName);
+}
+
+[DataContract]
+public class GrantInfo
+{
+    [DataMember]
+    public string GrantRequestKey { get; set; }
+
+    [DataMember]
+    public string GrantRequestDate { get; set; }
+
+    [DataMember]
+    public string PersonKey { get; set; }
+
+    [DataMember]
+    public string GrantTypeKey { get; set; }
+
+    [DataMember]
+    public string GrantRequestExplanation { get; set; }
+
+    [DataMember]
+    public string GrantRequestAmount { get; set; }
+
+    [DataMember]
+    public List<Person> GrantPerson { get; set; }
+}
